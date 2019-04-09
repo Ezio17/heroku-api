ca5521f70b2a15007d4525ad01f1467a2401a015
ezio17 <xd_ezio@ukr.net>
2019-04-09 19:08:46 +0300

    new changes

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict'
+
+let userList = document.getElementById('user-list')
+let form = document.getElementById('form')
+console.log(form)
+
+let usersList = [];
+render()
+
+form.onsubmit = (event) => {
+  event.preventDefault();
+
+  const user = {
+    firstName: form.elements.firstName.value,
+    lastName: form.elements.lastName.value,
+    email: form.elements.email.value,
+    password: form.elements.password.value
+  };
+
+  createUser(user)
+};
+
+function createUser(user) {
+  fetch('http://127.0.0.1:3000/users', {
+    method: 'POST',
+    body: JSON.stringify(user),
+  })
+    .then(res => res.json())
+    .then(data => {
+      usersList = data;
+      render();
+    });
+}
+
+
+fetch('http://127.0.0.1:3000/users')
+  .then(response => response.json())
+  .then(users => {
+    usersList = users
+    render()
+  })
+
+function render() {
+  if (usersList.length === 0) {
+    return
+  }
+
+  userList.innerHTML = ` 
+  <tr>
+   <th class="th">First Name</th>
+   <th class="th">Last Name</th>
+   <th class="th">Email</th>
+   <th class="th">Password</th>
+  <tr>
+  ${
+    usersList.map(user => `
+    <tr>
+     <td class="td">${user.firstName}</td>
+     <td class="td">${user.lastName}</td>
+     <td class="td">${user.email}</td>
+     <td class="td">${user.password}</td>
+   </tr>
+  `).join('')
+    }`
+}
+
--- server.js
+++ server.js
@@ -1,18 +1,45 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const fs = require('fs')
 const port = process.env.PORT || 3000
-
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.use(bodyParser.text())
+
+let users = []
+
+fs.readFile('./users.json', 'utf8', function (err, data) {
+  if (err) {
+    return;
+  }
+
+  users = JSON.parse(data)
 });
 
-app.post("/users", urlencodedParser, function (request, response) {
+function writeUsers() {
+  fs.writeFile('./users.json', JSON.stringify(users), function (err) {
+    if (err) {
+      return;
+    }
+  });
+}
+
+app.get("/", function (request, response) {
+  response.send('Hello World!');
+});
+
+app.get('/users', (request, response) => {
+  response.set('Access-Control-Allow-Origin', '*');
+  response.json(users)
+});
+
+app.post("/users", function (request, response) {
   if (!request.body) return response.sendStatus(400);
-  console.log(request.body);
-  response.send(`Name: ${request.body.name} / Surname: ${request.body.surname}`);
+  users.push(JSON.parse(request.body))
+  writeUsers()
+
+  response.set('Access-Control-Allow-Origin', '*');
+  response.json(users);
 });
 
 app.listen(port, () => console.log(`Example app listening on http://127.0.0.1:${port}`))
