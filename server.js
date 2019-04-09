389690b20490910a41eefb21a37ff044180d78da
ezio17 <xd_ezio@ukr.net>
2019-04-09 09:54:38 +0300

    fix 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get("/", function (request, response) {
 app.post("/users", urlencodedParser, function (request, response) {
   if (!request.body) return response.sendStatus(400);
   console.log(request.body);
-  response.send(`Name: ${request.body.name}`);
+  response.send(`Name: ${request.body.name} / Surname: ${request.body.surname}`);
 });
 
 app.listen(port, () => console.log(`Example app listening on http://127.0.0.1:${port}`))
